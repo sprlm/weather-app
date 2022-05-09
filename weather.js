d78d1a7554e2842b8ddfefa73ff0e1743879617a
Matthew <matthewtoro17@gmail.com>
2022-05-09 17:54:05 +0800

    Add function that processes weather JSON

--- weather.js
+++ weather.js
@@ -1,9 +1,25 @@
-async function getWeather(location) {
-  try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=7458751a8ea08978c503e82b15abe794`);
-    const weatherData = await response.json();
-    console.log(weatherData);
-  } catch (err) {
-    console.error(err);
+async function getWeatherData(location) {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=7458751a8ea08978c503e82b15abe794&units=${units}`);
+  
+  if (response.ok) {
+    const weatherJSON = await response.json();
+    const weatherData = processWeatherJSON(weatherJSON);
+    return weatherData;
+  } else {
+    throw new Error(`Error ${response.status}`);
   }
 }
+
+function processWeatherJSON(weatherJSON) {
+  return {
+    name: weatherJSON.name,
+    temp: weatherJSON.main.temp,
+    weather: weatherJSON.weather[0].main,
+    description: weatherJSON.weather[0].description,
+    icon: `https://openweathermap.org/img/wn/${weatherJSON.weather[0].icon}@2x.png`
+  };
+}
+
+const units = 'metric';
+
+getWeatherData('san francisco').then(data => console.log(data));
